9d98a30127c302cfffb350327e08f5390947266d
kartikk0810 <165395756+kartikk0810@users.noreply.github.com>
2025-07-02 22:36:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use Strict"; // treat all js code as newer version
+
+alert ( 3+3); // this will not work in node but its fine to use this in browser 
+
+console.log(3 +3);
+
+console.log("Hello World");
+
+// data types 
+
+// primitive data types 
+
+ let age = 18; // this is numeric or number data type
+
+ let bigInt = 12345678000000; // in this we can store a large number for example a bank account number or multiple values which are bigger 
+ let name = "Hello World"; // this is string data type
+
+ let isLoggedin = true; // this is boolean data type indicating value in true or false 
+
+ null = standAlonevalue // this is called standalone value
+
+ let city; // this is undefined datatype in this we can define data later on
+
+ // non primitive data types
+
+
+ //object
+
+console.log(typeof undefined); // it returns value or typeof data is undefined
+
+console.log(typeof null); // it returns object
