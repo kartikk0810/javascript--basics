347305a1f60d943f3fc286ddcf7c6b7c3e69f865
kartikk0810 <165395756+kartikk0810@users.noreply.github.com>
2025-07-02 23:20:37 +0000

    conversion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,39 @@
+let score = "Hello World";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+// "33" = 33 string value converted in number
+// "33abc" = NaN it shows nan means not a number this will convert into number but stills it shows not a number
+// true = 1 , false = 0 boolean value conversion into number 
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// when we store value 0 that means we stored value in  number and when its converted to boolean it shows false
+
+// and when we stored value 1 that also means we stored value in number and its converted to boolean it shows true
+
+// same we can do in string also convert into boolean data type 
+
+
+// if we store empty string like this "" it passes false 
+
+// if we store data in string like this "Hello World" it passes the value true 
+
+let someNumber = 33;
+let StringNumber = String(someNumber);
+
+console.log(someNumber);
+console.log(typeof StringNumber);
+
+// converting number into string  example above
+
+
