46cddb69b2a3c7756a242af959b6557280eedf0e
kartikk0810 <165395756+kartikk0810@users.noreply.github.com>
2025-07-10 10:11:03 +0000

    Math and Num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,50 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 24.559
+// console.log(otherNumber.toPrecision(1));
+
+// const hundreds = 10000000;
+
+// console.log(hundreds.toLocaleString());
+
+
+// console.log(hundreds.toLocaleString('en-in'));
+
+
+//++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-44));
+// console.log(Math.round(4.9));
+// console.log(Math.floor(4));
+// console.log(Math.ceil(3.9));
+// console.log(Math.min(0 , 3 , 4 , 5));
+// console.log(Math.max(0 , 1 , 9 , 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log((Math.floor(Math.random() * 10)) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
